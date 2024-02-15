443b61fe9e1e336419d9488d4859ea4a438253bb
tornikegabarashvili <tornikegabara@gmail.com>
2024-02-15 15:08:22 +0400

    third commit

---
+++ python/day 03/lesson3.py
@@ -0,0 +1,27 @@
+print("my fammily ages")
+
+grandpa = "grandpa 73"
+grandma = "grandma 63"
+dad = "dad 40"
+mom = "mom 36"
+me = "me 15"
+sister = "sister 13"
+
+print(grandpa)
+print(grandma)
+print(dad)
+print(me)
+print(sister)
+
+print("")
+
+print("after 2 years")
+
+print("")
+
+print("grandpa 75")
+print("grandma 65")
+print("dad 42")
+print("mom 38")
+print("me 17")
+print("sister 15")
